1ced13ff3ae1091f0aae1923eb53d442862a7bc7
prasoondev <prasoon5050@outlook.com>
2025-01-16 12:41:26 +0530

    dummy

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const jwt = require("jsonwebtoken");
 
 dbConnect();
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 const PORT = 3000;
 
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react"
+import axios from "axios";
 function Login() {
-    return (
-      <div>
-        <h1>Login</h1>
-        <form action="POST">
-          <input type="text" name="username" placeholder="Username" />
-          <input type="password" name="password" placeholder="Password" />
-          <input type="submit" />
-        </form>
-      </div>
-    )
+  const LOGIN_URL = 'http://localhost:3000/login';
+  // const REGISTER_URL = "http://localhost:3000/register";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
+  const configuration = {
+    method: "post",
+    url: LOGIN_URL,
+    data: {
+      email,
+      password,
+    },
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios(configuration)
+      .then((result) => {
+        console.log(result);
+        setLogin(true);
+      })
+      .catch((error) => {
+        error = new Error();
+        console.log(error);
+      });
   }
-  
-  export default Login
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="POST">
+        <input type="text" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input type="submit" value="Login" onClick={(e) => handleSubmit(e)} />
+      </form>
+    </div>
+  )
+}
+
+export default Login
